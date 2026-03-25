4dfd0c9ee7b74e2f3b19ec793d4b371a561f1c74
gavdov38 <gavin.a.docherty@gmail.com>
2026-03-25 19:54:04 +0000

    fs: susfs: changes and bug fixes for OPEN_REDIRECT, SUS_MAP, and memory allocation - For OPEN_REDIRECT, there is no need to do put_unused_fd() again. Also rename function signature "int susfs_open_redirect_spoof_do_sys_openat(struct inode *inode, struct filename **tmp_filename);" to "struct filename *susfs_open_redirect_spoof_do_sys_openat(struct inode *inode);". - For OPEN_REDIRECT, use `goto out;` instead of having `kfree(tmp);` and `return len;` on `base.c`. This will fix the kernel panic, system crashes, and force reboot triggered on some apps after using OPEN_REDIRECT. - For memeory allocation in fs/susfs.c, we should use kzalloc() to allocate new memory or explictly zero out the last char buffer after strncpy() if buffer is allocated via kmalloc(). - Use PRE_CHECK_SUS_MAP(inode) macro to reduce tedious repeated code - Sync Some SUS_MAP Code from upstream

--- fs/namei.c
+++ fs/namei.c
@@ -4982,7 +4982,7 @@ int generic_readlink(struct dentry *dentry, char __user *buffer, int buflen)
 			return PTR_ERR(link);
 	}
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-	if (PRE_CHECK_OPEN_REDIRECT(inode)) {
+	if (SUSFS_IS_INODE_OPEN_REDIRECT(inode)) {
 		res = susfs_open_redirect_spoof_vfs_readlink(inode, buffer, buflen);
 		if (!res) {
 			do_delayed_call(&done);
@@ -5017,7 +5017,7 @@ int vfs_readlink(struct dentry *dentry, char __user *buffer, int buflen)
 		if (unlikely(inode->i_op->readlink))
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 		{
-			if (PRE_CHECK_OPEN_REDIRECT(inode)) {
+			if (SUSFS_IS_INODE_OPEN_REDIRECT(inode)) {
 				res = susfs_open_redirect_spoof_vfs_readlink(inode, buffer, buflen);
 				if (!res)
 					return res;
